ee1122e1c5d742655b7a4bad440c3d625871390d
Brendan Campbell <bdcam@umich.edu>
2020-02-16 23:08:11 -0500

    Initial Commit

---
+++ birds-eye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  birds-eye
+//
+//  Created by Brendan Campbell on 2/16/20.
+//  Copyright © 2020 eecs497. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
